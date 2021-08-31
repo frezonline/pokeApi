fb757e9e5ea23a225c3e60b615e34c40f873dc4f
frezonline <55662236+frezonline@users.noreply.github.com>
2021-09-01 01:35:43 +0300

    v

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const selectors = {
 const AddBtn = document.querySelector(selectors.AddBtn);
 
 const baseUrl = 'https://pokeapi.co/api/v2/pokemon/'
-const imgUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+const imgUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/'
 const person = document.getElementById('app');
 
 AddBtn.addEventListener("click", function() {
@@ -16,7 +16,9 @@ AddBtn.addEventListener("click", function() {
 		console.log(json);
 		person.innerHTML = `
 		<div class="infoPokemon">
-		<img id=${randomСharacter} class="imgPokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomСharacter}.png"</img>
+		<div class="imgPoke">
+		<img id=${randomСharacter} class="imgPokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${randomСharacter}.png"</img>
+		</div>
 		<h1 id=${randomСharacter} class="namePokemon">${json.name}</h1>
 		</div>
 		`
